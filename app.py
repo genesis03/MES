9b333fe9e696740bd92daa49f1327050baa0a7f5
genesis03 <57065733+genesis03@users.noreply.github.com>
2026-09-14 14:21:22 +0900

    외주가공 출고 API 라우터 연결

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import models  # 기존 테이블 자동 생성 트리거
 import models.partner  # 신규 거래처 테이블 자동 생성 트리거
 from core.security import init_default_accounts
 
-from routers import pages, manual, shipping, basic_info, bom, partner, admin, auth, purchase, purchase_pages, purchase_inquiry, purchase_edit, subcontract, subcontract_pages, subcontract_inquiry
+from routers import pages, manual, shipping, basic_info, bom, partner, admin, auth, purchase, purchase_pages, purchase_inquiry, purchase_edit, subcontract, subcontract_pages, subcontract_inquiry, subcontract_outbound
 
 # 초기 계정 데이터 생성 트리거
 init_default_accounts()
@@ -36,6 +36,7 @@ app.include_router(purchase_inquiry.router)
 app.include_router(purchase_edit.router)
 app.include_router(subcontract.router)
 app.include_router(subcontract_inquiry.router)
+app.include_router(subcontract_outbound.router)
 
 if __name__ == "__main__":
     import uvicorn
