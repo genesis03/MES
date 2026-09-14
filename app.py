c89aecc7b6f6251d5de47584c6fe86ae34fde0bc
genesis03 <57065733+genesis03@users.noreply.github.com>
2026-09-15 00:55:53 +0900

    Register production run API

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import models  # 기존 테이블 자동 생성 트리거
 import models.partner  # 신규 거래처 테이블 자동 생성 트리거
 from core.security import init_default_accounts
 
-from routers import pages, manual, shipping, basic_info, basic_info_workers, basic_info_equipment, bom, partner, admin, auth, purchase, purchase_pages, purchase_inquiry, purchase_edit, subcontract, subcontract_pages, subcontract_inquiry, subcontract_outbound, subcontract_inbound, purchase_unreceived, quality_pages, quality, production_pages, production
+from routers import pages, manual, shipping, basic_info, basic_info_workers, basic_info_equipment, bom, partner, admin, auth, purchase, purchase_pages, purchase_inquiry, purchase_edit, subcontract, subcontract_pages, subcontract_inquiry, subcontract_outbound, subcontract_inbound, purchase_unreceived, quality_pages, quality, production_pages, production, production_run
 
 # 초기 계정 데이터 생성 트리거
 init_default_accounts()
@@ -46,6 +46,7 @@ app.include_router(quality_pages.router)
 app.include_router(quality.router)
 app.include_router(production_pages.router)
 app.include_router(production.router)
+app.include_router(production_run.router)
 
 if __name__ == "__main__":
     import uvicorn
